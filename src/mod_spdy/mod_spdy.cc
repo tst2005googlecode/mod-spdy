cd084cfb64770b8460bad48cff39f3d277c45fba
bmcquade@google.com <bmcquade@google.com@96a8b0a0-ff0d-11de-b9d1-2d015cf73129>
2010-01-13 01:36:54 +0000

    Clean up includes.

--- src/mod_spdy/mod_spdy.cc
+++ src/mod_spdy/mod_spdy.cc
@@ -1,17 +1,9 @@
 extern "C" {
-#include "apr_buckets.h"
-#include "apr_strings.h"
-#include "ap_config.h"
-#include "util_filter.h"
 #include "httpd.h"
 #include "http_config.h"
-#include "http_request.h"
-#include "http_core.h"
-#include "http_protocol.h"
 #include "http_log.h"
-#include "http_main.h"
-#include "util_script.h"
-#include "http_core.h"                                                             
+#include "http_request.h"
+#include "util_filter.h"
 }
 
 namespace {
@@ -19,12 +11,12 @@ namespace {
 ap_filter_rec_t* g_spdy_output_filter;
 
 int spdy_init(ap_filter_t *f) {
-    if (f->c) ap_log_cerror(__FILE__, __LINE__, APLOG_NOTICE, APR_SUCCESS, f->c, "Init");
+    ap_log_cerror(__FILE__, __LINE__, APLOG_NOTICE, APR_SUCCESS, f->c, "Init");
     return APR_SUCCESS;
 }
 
 apr_status_t spdy_filter(ap_filter_t *f, apr_bucket_brigade *bb) {
-    ap_log_cerror(__FILE__, __LINE__, APLOG_NOTICE, APR_SUCCESS, f->c, "Here I am.");
+    ap_log_cerror(__FILE__, __LINE__, APLOG_NOTICE, APR_SUCCESS, f->c, "Filter");
     return ap_pass_brigade(f->next, bb);
 }
 
@@ -36,10 +28,11 @@ void spdy_insert_filter_hook(request_rec *r) {
 void spdy_register_hook(apr_pool_t *p) {
     ap_hook_insert_filter(spdy_insert_filter_hook, NULL, NULL, APR_HOOK_MIDDLE) ;
 
-    g_spdy_output_filter = ap_register_output_filter("SPDY",
-                                                     spdy_filter,
-                                                     spdy_init,
-                                                     static_cast<ap_filter_type>(AP_FTYPE_PROTOCOL + 4));
+    g_spdy_output_filter = ap_register_output_filter(
+        "SPDY",
+        spdy_filter,
+        spdy_init,
+        static_cast<ap_filter_type>(AP_FTYPE_PROTOCOL + 4));
 }
 
 }  // namespace
