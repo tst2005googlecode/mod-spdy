1169ec25b8ae42c6618c000aaff90a6d7dab3507
bmcquade@google.com <bmcquade@google.com@96a8b0a0-ff0d-11de-b9d1-2d015cf73129>
2010-02-03 20:28:21 +0000

    Roll back unintended LOG messages.

--- src/mod_spdy/apache/http_stream_accumulator.cc
+++ src/mod_spdy/apache/http_stream_accumulator.cc
@@ -112,8 +112,6 @@ void HttpStreamAccumulator::OnStatusLine(const char *method,
                   version,
                   kCRLF);
 
-  LOG(INFO) << method << " " << path << " " << version;
-
   CHECK(strlen(uri.hostname) > 0);
   OnHeader(kHost, uri.hostname);
 }
@@ -132,8 +130,6 @@ void HttpStreamAccumulator::OnHeader(const char *key, const char *value) {
                   kHeaderSeparator,
                   value,
                   kCRLF);
-
-  LOG(INFO) << key << ": " << value;
 }
 
 void HttpStreamAccumulator::OnHeadersComplete() {
@@ -141,8 +137,6 @@ void HttpStreamAccumulator::OnHeadersComplete() {
                           apr_bucket_immortal_create(kCRLF,
                                                      kCRLFLen,
                                                      brigade_->bucket_alloc));
-
-  LOG(INFO) << "headers complete";
 }
 
 void HttpStreamAccumulator::OnBody(const char *data, size_t data_len) {
