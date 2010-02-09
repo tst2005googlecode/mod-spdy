8d5f85164db32eb17bc08434b7145774c29c9030
mdsteele@google.com <mdsteele@google.com@96a8b0a0-ff0d-11de-b9d1-2d015cf73129>
2010-02-09 19:36:11 +0000

    Fix C++ style issues in mod_static.cc
    r=lsong

--- src/mod_static/mod_static.cc
+++ src/mod_static/mod_static.cc
@@ -12,15 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern "C" {
-#include "httpd.h"
-#include "http_config.h"
-#include "http_request.h"
-#include "http_protocol.h"
-#include "http_log.h"
-}
-
 #include <string>
+
+#include "third_party/apache_httpd/include/httpd.h"
+#include "third_party/apache_httpd/include/http_config.h"
+#include "third_party/apache_httpd/include/http_log.h"
+#include "third_party/apache_httpd/include/http_protocol.h"
+#include "third_party/apache_httpd/include/http_request.h"
 
 namespace {
 
@@ -31,51 +29,54 @@ int static_default_handler(const std::string& filename, request_rec* r) {
   apr_table_setn(r->headers_out, "Set-Cookie", "mod_static=1.000");
   ap_rputs("<html><head><title>Wow, cache server</title></head>", r);
   ap_rputs("<body><h1>Cache Server is running</h1>OK Daniel Song", r);
-  ap_rputs("<hr>",r);
+  ap_rputs("<hr>", r);
   ap_rputs(filename.c_str(), r);
   ap_rputs("</body></html>", r);
 
   return OK;
 }
 
-const char* get_next_line(const char* buffer, int& size, std::string& line) {
-  line.clear();
-  if ( buffer == NULL) return NULL;
+const char* get_next_line(const char* buffer, int* size, std::string* line) {
+  line->clear();
+  if (buffer == NULL) {
+    return NULL;
+  }
 
   const char* pointer = buffer;
-  for (; size >0 ; --size, ++pointer) {
-    if (*pointer == '\r' && *(pointer+1) == '\n') {
-      size -= 2;
-      return pointer+2;
+  for (; *size > 0; --*size, ++pointer) {
+    if (*pointer == '\r' && *(pointer + 1) == '\n') {
+      *size -= 2;
+      return pointer + 2;
     }
-    line.append(1, *pointer);
+    line->push_back(*pointer);
   }
   return NULL;
 }
 int process_file(const std::string& filename, request_rec* r) {
 
-  apr_file_t* fd=NULL;
-  apr_status_t rv = apr_file_open(&fd, filename.c_str(),
-                                  APR_READ|APR_SHARELOCK|APR_SENDFILE_ENABLED,
-                                  APR_OS_DEFAULT, r->pool);
+  apr_file_t* fd = NULL;
+  apr_status_t rv =
+      apr_file_open(&fd, filename.c_str(),
+                    APR_READ | APR_SHARELOCK | APR_SENDFILE_ENABLED,
+                    APR_OS_DEFAULT, r->pool);
   if (rv != APR_SUCCESS) {
-    ap_log_rerror(__FILE__,__LINE__,APLOG_ERR,0,r,
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_SUCCESS, r,
                   "Can't open %s", filename.c_str());
     //return HTTP_NOT_FOUND;
     return static_default_handler(filename, r);
   }
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO,0,r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                 "Opened %s", filename.c_str());
 
   apr_finfo_t finfo;
   rv = apr_file_info_get(&finfo, APR_FINFO_SIZE, fd);
   if (rv != APR_SUCCESS) {
-    ap_log_rerror(__FILE__,__LINE__,APLOG_ERR,0,r,
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_SUCCESS, r,
                   "Can't get file size of %s", filename.c_str());
     //return HTTP_NOT_FOUND;
     return static_default_handler(filename, r);
   }
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO,0,r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                 "Get file size=%d", finfo.size);
 
 //  apr_size_t sz;
@@ -93,7 +94,7 @@ int process_file(const std::string& filename, request_rec* r) {
   std::string line;
   bool first_line = true;
   while (in_header) {
-    header = get_next_line(header, size, line);
+    header = get_next_line(header, &size, &line);
     if ( first_line ) {
       if (line.compare(0, 8 ,"HTTP/1.0")) {
         apr_table_set(r->subprocess_env, "force-response-1.0", "1");
@@ -102,7 +103,7 @@ int process_file(const std::string& filename, request_rec* r) {
       continue;
     }
 
-    if( line.empty() ) {
+    if (line.empty()) {
       // empty line; end of header
       break;
     }
@@ -110,17 +111,17 @@ int process_file(const std::string& filename, request_rec* r) {
     // parse headers
     std::size_t pos = line.find(':');
     if (pos == std::string::npos) {
-      ap_log_rerror(__FILE__,__LINE__,APLOG_ERR,0,r,
+      ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_SUCCESS, r,
                    "Unkown header: %s", line.c_str());
     }
     std::string header = line.substr(0, pos);
-    std::string value = line.substr(pos+2); // skip ": "
+    std::string value = line.substr(pos + 2); // skip ": "
     apr_table_add(r->headers_out, header.c_str(), value.c_str());
-    ap_log_rerror(__FILE__,__LINE__,APLOG_INFO,0,r,
+    ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                  "%s: %s", header.c_str(), value.c_str());
   }
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO,0,r,
-               "Sending content %d bytes",size);
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
+               "Sending content %d bytes", size);
 
   // send the body
   ap_rwrite(header, size, r);
@@ -135,24 +136,24 @@ std::string get_request_filename(request_rec* r) {
   std::string filename;
 
   char buffer[5] = {0};
-  for (const char* uri = r->unparsed_uri; *uri; uri++) {
-    if ((*uri >='0' && *uri <='9') ||
-        (*uri >='A' && *uri <='Z') ||
-        (*uri >='a' && *uri <='z') ||
+  for (const char* uri = r->unparsed_uri; *uri; ++uri) {
+    if ((*uri >= '0' && *uri <= '9') ||
+        (*uri >= 'A' && *uri <= 'Z') ||
+        (*uri >= 'a' && *uri <= 'z') ||
         (*uri == '_') ||
         (*uri == '/')) {
       filename.append(1, *uri);
     } else {
-      snprintf(buffer,4, "x%X", *uri);
+      snprintf(buffer, 4, "x%X", *uri);
       filename.append(buffer);
     }
   }
 
-  if (filename.at(filename.size()-1) == '/') {
+  if (filename.at(filename.size() - 1) == '/') {
     filename.append("indexx2Ehtml");
   }
   // TODO use ServerRoot
-  std::string full_filename ("/usr/local/apache2/htdocs/static/");
+  std::string full_filename("/usr/local/apache2/htdocs/static/");
   full_filename.append(r->hostname);
   full_filename.append(filename);
   return full_filename;
@@ -160,31 +161,31 @@ std::string get_request_filename(request_rec* r) {
 
 int static_handler(request_rec* r) {
   // check if request for static
-  if (!r->handler || strcmp(r->handler,"static")) {
-    ap_log_rerror(__FILE__,__LINE__,APLOG_DEBUG, 0, r,
+  if (!r->handler || strcmp(r->handler, "static")) {
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r,
                   "Not static request.");
     return DECLINED;
   }
 
   // Only handle GET request
   if (r->method_number != M_GET) {
-    ap_log_rerror(__FILE__,__LINE__,APLOG_DEBUG, 0, r,
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r,
                   "Not GET request: %d.", r->method_number);
     return HTTP_METHOD_NOT_ALLOWED;
   }
 
   // TODO remove DEBUG info
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO, 0, r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                   "URI(unparsed): %s", r->unparsed_uri);
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO, 0, r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                   "URI: %s", r->uri);
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO, 0, r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                   "hostname: %s", r->hostname);
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO, 0, r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                   "filename: %s", r->filename);
 
   std::string full_filename = get_request_filename(r);
-  ap_log_rerror(__FILE__,__LINE__,APLOG_INFO, 0, r,
+  ap_log_rerror(APLOG_MARK, APLOG_INFO, APR_SUCCESS, r,
                   "full_filename: %s", full_filename.c_str());
 
 
