a69f1b896a5e8e1364755052bfbdfce2604d4045
mdsteele@google.com <mdsteele@google.com@96a8b0a0-ff0d-11de-b9d1-2d015cf73129>
2011-12-20 21:11:53 +0000

    Allocate our own socket object for slaves rather than reusing the master socket
    r=aoates

--- src/mod_spdy/apache/apache_spdy_stream_task_factory.cc
+++ src/mod_spdy/apache/apache_spdy_stream_task_factory.cc
@@ -24,6 +24,7 @@
 #undef CORE_PRIVATE
 
 #include "apr_buckets.h"
+#include "apr_network_io.h"
 #include "http_log.h"
 #include "util_filter.h"
 
@@ -55,7 +56,7 @@ class ApacheStreamTask : public net_instaweb::Function {
   SpdyStream* const stream_;
   LocalPool local_;
   conn_rec* const slave_connection_;  // allocated in local_.pool()
-  void* const csd_;
+  apr_socket_t* slave_socket_;
 
   DISALLOW_COPY_AND_ASSIGN(ApacheStreamTask);
 };
@@ -65,21 +66,7 @@ ApacheStreamTask::ApacheStreamTask(const conn_rec* master_connection,
     : stream_(stream),
       slave_connection_((conn_rec*)apr_pcalloc(local_.pool(),
                                                sizeof(conn_rec))),
-      // We're supposed to pass a socket object to ap_process_connection below,
-      // but there's no meaningful object to pass for this slave connection.
-      // Our pre-connection hook will prevent the core filters, which talk to
-      // the socket, from being inserted, so they won't notice anyway; but
-      // still, passing NULL to ap_process_connection might cause some modules
-      // other to segfault.  So, we'll use the socket object from the master
-      // connection, which is stored as the connection context for the core
-      // module.  Hopefully those modules won't try to read from the socket or
-      // alter its settings; if they just want to read its settings, it's
-      // probably mostly harmless to use the master connection's socket.
-      //
-      // TODO(mdsteele): But are socket objects thread-safe?  Maybe we should
-      //   allocate our own socket object.
-      csd_(ap_get_module_config(master_connection->conn_config,
-                                &core_module)) {
+      slave_socket_(NULL) {
   // Pick a globally-unique ID for the slave connection; this must be unique at
   // any given time.  Normally the MPM is responsible for assigning these, and
   // each MPM does it differently, so we're cheating in a dangerous way by
@@ -105,6 +92,7 @@ ApacheStreamTask::ApacheStreamTask(const conn_rec* master_connection,
   // TODO(mdsteele): We could also consider using an #if here to widen the
   //   masks and the shift distance on systems where sizeof(long)==8.  We might
   //   as well use those extra bits if we have them.
+  COMPILE_ASSERT(sizeof(long) >= 4, long_is_at_least_32_bits);
   const long slave_connection_id =
       -(((master_connection->id & 0x7fffL) << 16) |
         (static_cast<long>(stream->stream_id()) & 0xffffL));
@@ -128,6 +116,20 @@ ApacheStreamTask::ApacheStreamTask(const conn_rec* master_connection,
   slave_connection_->local_ip = master_connection->local_ip;
   slave_connection_->remote_addr = master_connection->remote_addr;
   slave_connection_->remote_ip = master_connection->remote_ip;
+
+  // We're supposed to pass a socket object to ap_process_connection below, but
+  // there's no meaningful object to pass for this slave connection, because
+  // we're not really talking to the network.  Our pre-connection hook will
+  // prevent the core filters, which talk to the socket, from being inserted,
+  // so they won't notice anyway; nonetheless, we can't pass NULL to
+  // ap_process_connection because that can cause some other modules to
+  // segfault if they try to muck with the socket's settings.  So, we'll just
+  // allocate our own socket object for those modules to mess with.  This is a
+  // kludge, but it seems to work.
+  const apr_status_t status = apr_socket_create(
+      &slave_socket_, APR_INET, SOCK_STREAM, APR_PROTO_TCP, local_.pool());
+  DCHECK(status == APR_SUCCESS);
+  DCHECK(slave_socket_ != NULL);
 }
 
 void ApacheStreamTask::Run() {
@@ -143,20 +145,21 @@ void ApacheStreamTask::Run() {
     ConnectionContext* context = CreateSlaveConnectionContext(
         slave_connection_, stream_);
 
-    // Next, we have a further workaround.  Normally, the core pre-connection
-    // hook sets the core module's connection context to the csd passed to
-    // ap_process_connection; certain other modules, such as mod_reqtimeout,
-    // read the core module's connection context directly so as to read this
-    // socket's settings.  However, we purposely don't allow the core
-    // pre-connection hook to run, because we don't want the core connection
-    // filters to be inserted.  To avoid breaking other modules, we take it
-    // upon oursevles to set the core module's connection context to the csd we
-    // are passing to ap_process_connection.  This is ugly, but seems to work.
-    ap_set_module_config(slave_connection_->conn_config, &core_module, csd_);
+    // Normally, the core pre-connection hook sets the core module's connection
+    // context to the socket passed to ap_process_connection; certain other
+    // modules, such as mod_reqtimeout, read the core module's connection
+    // context directly so as to read this socket's settings.  However, we
+    // purposely don't allow the core pre-connection hook to run, because we
+    // don't want the core connection filters to be inserted.  So, to avoid
+    // breaking other modules, we take it upon oursevles to set the core
+    // module's connection context to the socket we are passing to
+    // ap_process_connection.  This is ugly, but seems to work.
+    ap_set_module_config(slave_connection_->conn_config,
+                         &core_module, slave_socket_);
 
     // Invoke Apache's usual processing pipeline.  This will block until the
     // connection is complete.
-    ap_process_connection(slave_connection_, csd_);
+    ap_process_connection(slave_connection_, slave_socket_);
   }
 
   ap_log_cerror(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, slave_connection_,
